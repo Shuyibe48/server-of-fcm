d4542bcc6074e16328e8d7b2dd55bdc5b1a64143
Shuyibe <shuyibesiddikif@gmail.com>
2024-08-30 14:20:31 +0600

    package install

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const morgan = require("morgan");
+require("dotenv").config();
+const bodyParser = require("body-parser");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = 5000;
 
 app.get('/about', (req, res) => {
   res.send({about: 'This is about page!'})
