336253125de8f50fb3b9a3340cb33ffbab85f791
Shuyibe <shuyibesiddikif@gmail.com>
2024-08-30 14:14:41 +0600

    about

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 5000
 
+app.get('/about', (req, res) => {
+  res.send({about: 'This is about page!'})
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
